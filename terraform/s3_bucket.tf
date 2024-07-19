46eb427d661d2af6da49f2012259fe15e5bd4b7d
nehapatil-coditas <159018597+nehapatil-coditas@users.noreply.github.com>
2024-07-19 15:44:12 +0530

    Update s3_bucket.tf

--- terraform/s3_bucket.tf
+++ terraform/s3_bucket.tf
@@ -5,6 +5,7 @@ resource "aws_s3_bucket" "bucket" {
   force_destroy = true
 }
 
+# Versioning Enabled
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
