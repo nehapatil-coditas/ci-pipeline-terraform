d50f32a512380dd4e68880a3a0c3537ce363d592
nehapatil-coditas <159018597+nehapatil-coditas@users.noreply.github.com>
2024-07-19 15:37:01 +0530

    Update sns_topic.tf

--- terraform/sns_topic.tf
+++ terraform/sns_topic.tf
@@ -3,7 +3,6 @@ resource "aws_sns_topic" "instance_updates" {
   name = "instance-updates-topic"
 }
 
-# Creating sns topic subscription
 resource "aws_sns_topic_subscription" "instance_updates_sqs_target" {
   topic_arn = aws_sns_topic.instance_updates.arn
   protocol  = "email"
